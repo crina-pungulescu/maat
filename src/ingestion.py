8dabcc42e1480310b57e6bd663b9e014404fde42
Crina Pungulescu <ymkjtgpmdt@privaterelay.appleid.com>
2026-05-12 20:56:21 +0200

    Update ingestion.py

--- src/ingestion.py
+++ src/ingestion.py
@@ -257,7 +257,7 @@ def fetch_rss_articles():
             text = (entry.get("title", "") + " " + entry.get("summary", "")).strip()
 
             article = {
-                "article_id": make_article_id(article)
+                "article_id": make_article_id(article),
                 "title": entry.get("title", ""),
                 "link": entry.get("link", ""),
                 "published": entry.get("published", ""),
